fb546aa0a44eb18f1c26001dcf7a46835437ae6a
93simon <93simonster@gmail.com>
2022-12-30 12:19:59 +0100

    Analyze Humidity & Air Quality Data

--- sensor/particle_count_info.py
+++ sensor/particle_count_info.py
@@ -17,14 +17,15 @@ class ParticleData(HouseInfo):
         recs = super().get_data_by_date("particulate", rec_date)
         return self._convert_data(recs)
 
-    def get_data_concentration(self, data):
+    def get_data_concentrations(self, data):
         particulate = {"good": 0, "moderate": 0, "bad": 0}
-
         for rec in data:
-            if rec < 50.0:
+            # Select particulate concentration
+            if rec <= 50.0:
                 particulate["good"] += 1
-            elif 50.0 < rec < 100.0:
+            elif rec > 50.0 and rec <= 100:
                 particulate["moderate"] += 1
             else:
                 particulate["bad"] += 1
+
         return particulate
